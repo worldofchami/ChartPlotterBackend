4b92fc55bc8dbddb68f8de55b3b5bf1166c510a3
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2024-04-03 20:26:49 +0200

    Tweaks to fix deployment

--- app/main.py
+++ app/main.py
@@ -25,5 +25,4 @@ def generate(name):
             "message": "Error generating chart. Please try again!"
         })
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
